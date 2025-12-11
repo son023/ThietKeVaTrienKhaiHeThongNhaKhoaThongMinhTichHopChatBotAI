c35579ebd6233b75ff29934fc7cb513092acb785
son023 <sonnguyenvan122003@gmail.com>
2025-12-12 00:06:37 +0700

    add docker setup complete
    # Conflicts:
    #	scripts/doctor-service.sql
    #	scripts/insurance-service.sql
    #	scripts/inventory-service.sql
    #	scripts/invoice-service.sql
    #	scripts/patient-service.sql
    #	scripts/payment-service.sql
    #	service/docker-compose.yml
    #	service/invoice-service/build.gradle
    #	service/prescription-service/build.gradle

--- scripts/user-service.sql
+++ scripts/user-service.sql
@@ -86,3 +86,94 @@ INSERT INTO user_role (id, user_id, role_id) VALUES
 ('00000000-0000-0000-0000-000000001008', '00000000-0000-0000-0000-000000000501', '00000000-0000-0000-0000-000000000006');
 
 END $$;
+CREATE TABLE IF NOT EXISTS public.token_entry
+(
+    processor_name character varying(255) COLLATE pg_catalog."default" NOT NULL,
+    segment integer NOT NULL,
+    owner character varying(255) COLLATE pg_catalog."default",
+    "timestamp" character varying(255) COLLATE pg_catalog."default" NOT NULL,
+    token oid,
+    token_type character varying(255) COLLATE pg_catalog."default",
+    CONSTRAINT token_entry_pkey PRIMARY KEY (processor_name, segment)
+)
+
+TABLESPACE pg_default;
+
+CREATE TABLE IF NOT EXISTS public.saga_entry
+(
+    saga_id character varying(255) COLLATE pg_catalog."default" NOT NULL,
+    revision character varying(255) COLLATE pg_catalog."default",
+    saga_type character varying(255) COLLATE pg_catalog."default",
+    serialized_saga oid,
+    CONSTRAINT saga_entry_pkey PRIMARY KEY (saga_id)
+)
+
+TABLESPACE pg_default;
+
+CREATE TABLE IF NOT EXISTS public.dead_letter_entry
+(
+    dead_letter_id character varying(255) COLLATE pg_catalog."default" NOT NULL,
+    cause_message character varying(1023) COLLATE pg_catalog."default",
+    cause_type character varying(255) COLLATE pg_catalog."default",
+    diagnostics oid,
+    enqueued_at timestamp(6) with time zone NOT NULL,
+    last_touched timestamp(6) with time zone,
+    aggregate_identifier character varying(255) COLLATE pg_catalog."default",
+    event_identifier character varying(255) COLLATE pg_catalog."default" NOT NULL,
+    message_type character varying(255) COLLATE pg_catalog."default" NOT NULL,
+    meta_data oid,
+    payload oid NOT NULL,
+    payload_revision character varying(255) COLLATE pg_catalog."default",
+    payload_type character varying(255) COLLATE pg_catalog."default" NOT NULL,
+    sequence_number bigint,
+    time_stamp character varying(255) COLLATE pg_catalog."default" NOT NULL,
+    token oid,
+    token_type character varying(255) COLLATE pg_catalog."default",
+    type character varying(255) COLLATE pg_catalog."default",
+    processing_group character varying(255) COLLATE pg_catalog."default" NOT NULL,
+    processing_started timestamp(6) with time zone,
+    sequence_identifier character varying(255) COLLATE pg_catalog."default" NOT NULL,
+    sequence_index bigint NOT NULL,
+    CONSTRAINT dead_letter_entry_pkey PRIMARY KEY (dead_letter_id),
+    CONSTRAINT ukhlr8io86j74qy298xf720n16v UNIQUE (processing_group, sequence_identifier, sequence_index)
+)
+
+TABLESPACE pg_default;
+
+
+CREATE INDEX IF NOT EXISTS idxe67wcx5fiq9hl4y4qkhlcj9cg
+    ON public.dead_letter_entry USING btree
+    (processing_group COLLATE pg_catalog."default" ASC NULLS LAST)
+    WITH (fillfactor=100, deduplicate_items=True)
+    TABLESPACE pg_default;
+
+CREATE INDEX IF NOT EXISTS idxrwucpgs6sn93ldgoeh2q9k6bn
+    ON public.dead_letter_entry USING btree
+    (processing_group COLLATE pg_catalog."default" ASC NULLS LAST, sequence_identifier COLLATE pg_catalog."default" ASC NULLS LAST)
+    WITH (fillfactor=100, deduplicate_items=True)
+    TABLESPACE pg_default;
+
+CREATE TABLE IF NOT EXISTS public.association_value_entry
+(
+    id bigint NOT NULL,
+    association_key character varying(255) COLLATE pg_catalog."default" NOT NULL,
+    association_value character varying(255) COLLATE pg_catalog."default",
+    saga_id character varying(255) COLLATE pg_catalog."default" NOT NULL,
+    saga_type character varying(255) COLLATE pg_catalog."default",
+    CONSTRAINT association_value_entry_pkey PRIMARY KEY (id)
+)
+
+TABLESPACE pg_default;
+
+
+CREATE INDEX IF NOT EXISTS idxgv5k1v2mh6frxuy5c0hgbau94
+    ON public.association_value_entry USING btree
+    (saga_id COLLATE pg_catalog."default" ASC NULLS LAST, saga_type COLLATE pg_catalog."default" ASC NULLS LAST)
+    WITH (fillfactor=100, deduplicate_items=True)
+    TABLESPACE pg_default;
+
+CREATE INDEX IF NOT EXISTS idxk45eqnxkgd8hpdn6xixn8sgft
+    ON public.association_value_entry USING btree
+    (saga_type COLLATE pg_catalog."default" ASC NULLS LAST, association_key COLLATE pg_catalog."default" ASC NULLS LAST, association_value COLLATE pg_catalog."default" ASC NULLS LAST)
+    WITH (fillfactor=100, deduplicate_items=True)
+    TABLESPACE pg_default;
