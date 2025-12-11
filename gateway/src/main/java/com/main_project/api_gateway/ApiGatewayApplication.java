16ea4f6e47c4ec548ee6f66340c648c190e07020
Hung <hungthatsu@gmail.com>
2025-12-11 17:33:37 +0700

    sửa giao diện fe

--- gateway/src/main/java/com/main_project/api_gateway/ApiGatewayApplication.java
+++ gateway/src/main/java/com/main_project/api_gateway/ApiGatewayApplication.java
@@ -11,7 +11,6 @@ import org.springframework.web.reactive.config.EnableWebFlux;
 @EnableFeignClients
 @EnableWebFlux
 public class ApiGatewayApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(ApiGatewayApplication.class, args);
 	}
